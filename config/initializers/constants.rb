223a58e5143dcdc587f98ef92bdaf9fb30da7c13
ldgarber <leah.garber88@gmail.com>
2016-11-29 22:23:30 -0500

    fixed time zone stuff hopefully

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,3 @@
 #constants to set start and end dates
-START_DATE = Date.new(2016,12,01)
-END_DATE = Date.new(2016,12,24)
- 
+START_DATE = Date.new(2016,12,01).in_time_zone("Eastern Time (US & Canada)")
+END_DATE = Date.new(2016,12,24).in_time_zone("Eastern Time (US & Canada)") 
